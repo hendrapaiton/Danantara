e7a25e5051c946dd92037903183247eb23e97a66
Hendra Dwi Saputra <hendra@usa.com>
2025-02-20 08:59:18 +0700

    Initialize Koin dependency injection with application and module setup

---
+++ app/src/main/java/cloud/hendra/danantara/HendraApplication.kt
@@ -0,0 +1,30 @@
+package cloud.hendra.danantara
+
+import android.app.Application
+import cloud.hendra.danantara.utils.module.networkModule
+import cloud.hendra.danantara.utils.module.repositoryModule
+import cloud.hendra.danantara.utils.module.serviceModule
+import cloud.hendra.danantara.utils.module.usecaseModule
+import cloud.hendra.danantara.utils.module.viewmodelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class HendraApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@HendraApplication)
+            modules(
+                listOf(
+                    networkModule,
+                    serviceModule,
+                    usecaseModule,
+                    repositoryModule,
+                    viewmodelModule,
+                )
+            )
+        }
+    }
+}
---
+++ app/src/main/java/cloud/hendra/danantara/utils/module/RepositoryModule.kt
@@ -0,0 +1,5 @@
+package cloud.hendra.danantara.utils.module
+
+import org.koin.dsl.module
+
+val repositoryModule = module {}
